c09d83febaa7743e1bd40e3ee6dbd3c7816e5e65
Saideep Goud <saideep2244@gmail.com>
2023-12-04 15:44:01 +0530

    Installed tailwindCSS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-xl font-bold text-blue-600 p-3 shadow-md border border-solid border-black" >
+      Hello World
     </div>
   );
 }
